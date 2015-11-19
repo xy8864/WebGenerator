fe5a22de904f54757c6435a3dc949c173f57cd06
ran <xy8864@163.com>
2015-11-19 16:21:42 +0800

    release 1.0

--- release/webGenerator-1.0/run.bat
+++ release/webGenerator-1.0/run.bat
@@ -1,2 +1,2 @@
-start /b /i  cmd
-java -Xmx512m -Xms512m -Xmn256m -Dfile.encoding=UTF-8 -jar webGenerator-1.0.0.jar
+java -Xmx512m -Xms512m -Xmn256m -Dfile.encoding=UTF-8 -jar webGenerator-1.0.jar
+pause
